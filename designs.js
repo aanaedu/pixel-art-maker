ee52cb7a5e01f2d3ac38e6c5f1d8ae320541ddb0
Anthony Anaedu <tony.anaedu@gmail.com>
2017-12-07 18:37:46 +0000

    add paint mode and clear box features

--- designs.js
+++ designs.js
@@ -1,4 +1,15 @@
+/* global  $ */
+
 $(function () {
+    
+    const $sizePicker = $('#sizePicker');
+    const $inputHeight = $('#input_height');
+    const $inputWidth = $('#input_width');
+    const $pixelCanvas = $('#pixel_canvas');
+    const $colorPicker = $('#colorPicker');
+    const $btnClearCanvas = $('#btn_clear');
+    const $btnPaintMode = $('#btn_paint_mode');
+    let isDragMode = false;
 
     function makeGrid(row, col) {
         let output = '';
@@ -11,21 +22,41 @@ $(function () {
         }
         return output;
     }
+    
+    function paintBox(e) {
+           const color = (e.button == 2) ? '#fff' : $colorPicker.val();
+            $(e.target).css("background-color", color);
+    }
 
-    $('#sizePicker').on('submit', function (e) {
+    $sizePicker.on('submit', function (e) {
         e.preventDefault();
-        const height = $('#input_height').val();
-        const width = $('#input_width').val();
-        $('#pixel_canvas').html(makeGrid(height, width));
+        const height = $inputHeight.val();
+        const width = $inputWidth.val();
+        $pixelCanvas.html(makeGrid(height, width));
+    });
+    
+    $pixelCanvas.on('contextmenu', function(e) {
+        e.preventDefault(); 
+    });
+    
+    $pixelCanvas.on('mouseup', 'td', function(e) {
+       paintBox(e);
+    });
+    
+    $pixelCanvas.on('mouseover', 'td', function(e) {
+        if(isDragMode){
+          paintBox(e);
+        }
     });
 
-    $('#pixel_canvas').on('click', 'td', function(e) {
-        const color = $('#colorPicker').val();
-        $(this).css("background-color", color);
-    });
-
-    $('#btn_clear').on('click', function(e){
+    $btnClearCanvas.on('click', function(e) {
         e.preventDefault();
-        $('#pixel_canvas').html('');
+        $pixelCanvas.html('');
+    });
+    
+    $btnPaintMode.on('click', function(e) {
+        const modeText = (isDragMode) ? 'Click to Paint' : 'Drag to Paint';
+        $(e.target).text(modeText);
+        isDragMode = !isDragMode;
     });
 });
